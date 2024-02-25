7bc05ac17c8b31619b6d2b84da961506b9e7c403
Oscar Trabazos Claveria <33747969+OTRABAZOS@users.noreply.github.com>
2024-02-25 15:13:34 +0100

    update testing.js

---
+++ testing/testing.js
@@ -0,0 +1,11 @@
+require("./lint")
+require("./basic")
+require("./andSimple")
+require("./orSimple")
+require('./mixMultiple')
+
+/*
+require("./andMultiple")
+require("./orMultiple")
+require('./mixMultiple')
+*/
